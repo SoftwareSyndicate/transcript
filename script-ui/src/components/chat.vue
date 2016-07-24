dc67e7d1a4deac3791726abb5186def6be06f35d
Omri Mor <omri_mor@msn.com>
2016-07-24 10:55:14 -0700

    added msg input and button

--- script-ui/src/components/chat.vue
+++ script-ui/src/components/chat.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="chat">
     <h1>{{ msg }}</h1>
+    <input type="text" placeholder="Add your message..." class="input-msg"></input>
+    <div class="btn btn-solid">Send</div>
   </div>
 </template>
 
@@ -23,7 +25,61 @@ export default {
 </script>
 
 <style>
+
+/*inherited style*/
+
 body {
   font-family: Helvetica, sans-serif;
 }
+
+/*script-ui style*/
+
+.input-msg {
+  width: 92%;
+  padding: 10px 12px;
+  margin-bottom: 0.8em;
+  font-size: 12px;
+  font-weight: 400;
+  border: 1px solid #EAEEF9;
+  border-radius: 2px;
+  border-style: solid;
+}
+
+.input-msg:focus {
+  border: 1px solid #11C362;
+  outline: none;
+  color: #4A4A4A;
+}
+
+.btn {
+  width: 92%;
+  padding: 10px 12px;
+  margin-bottom: 1em;
+  color: #14B143;
+  font-size: 12px;
+  font-weight: 300;
+  background: #ffffff;
+  border: 1px solid #11C362;
+  border-radius: 2px;
+}
+
+.btn-solid {
+  color: #ffffff;
+  background: #11C362;
+}
+
+.btn-solid:hover {
+  border-bottom: 2px solid #0DB158;
+}
+
+.btn-solid:active {
+  background: #0DB158;
+}
+
+.btn-solid:visited {
+  background: #11C362;
+  border-bottom: none;
+
+}
+
 </style>
