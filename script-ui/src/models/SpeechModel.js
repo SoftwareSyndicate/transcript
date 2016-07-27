4f09246081ec1546d6c581663c356b95f0b66572
yintothayang <yintothayang@gmail.com>
2016-07-26 19:56:06 -0700

    starting speechModel

---
+++ script-ui/src/models/SpeechModel.js
@@ -0,0 +1,36 @@
+import Notifications from '../services/NotificationService'
+
+class SpeechModel {
+  constructor(){
+    this.messages = [];
+  }
+
+  addMessage(message){
+    let newMessageKey = firebase.database().ref().child('messages').push().key;
+    let now = new Date().getTime();
+    let newMessage = {
+      id: newMessageKey,
+      message: message,
+      created_at: now,
+      updated_at: now,
+    };
+
+    var updates = {};
+    updates['/messages/' + newMessageKey] = newMessage;
+    return firebase.database().ref().update(updates);
+  }
+
+  watchAllMessages(){
+    this.messagesRef = firebase.database().ref('messages').orderByChild("created_at");
+    this.messagesRef.on('value', data => {
+      this.messages = [];
+      for(let key in data.val()){
+        this.messages.push(data.val()[key]);
+      }
+      console.log(this.messages);
+      Notifications.notify("ChatModel.messagesUpdated");
+    });
+  }
+
+}
+export default new ChatModel();
